e669c86ec753d4f4b0dc9a7bd61bc96a3a29f25c
Max Pittsley <max.pittsley@gmail.com>
2014-07-31 17:30:52 -0700

    -g requires sudo + symlink node binary

--- setup.sh
+++ setup.sh
@@ -2,10 +2,11 @@
 echo "Sudoing..."
 sudo apt-get update
 sudo apt-get install -y nodejs npm
-npm install -g coffee-script
+sudo npm install -g coffee-script
 chmod +x update-scripts.sh
 mv update-scripts.sh update-scripts
 chmod +x run.coffee
 mv run.coffee run
 npm install
+ln -s /usr/bin/nodejs /usr/bin/node
 exit 0
